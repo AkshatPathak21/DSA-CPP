46feb186a2767229a4b4bb015c740a5e1ab6ce6c
Akshat Pathak <akshat.pathak03@gmail.com>
2024-03-15 00:03:57 +0530

    Update standardTemplateLib.cpp
    Pairs in Utility lib

--- standardTemplateLib.cpp
+++ standardTemplateLib.cpp
@@ -1,13 +1,21 @@
 # include <iostream>
+# include <utility>
 using namespace std;
 
+void explainPairs(){
+    //Declaration of Pairs
+    pair<int, int> p = {1,3};
+    cout<<"The first element of pair is "<<p.first<<" and the second element is "<<p.second<<endl;
+
+    //Nested pair
+    pair <int , pair<string, int >> p2 = {10053 ,{"Akshat", 21}};
+    cout<<"REg No.: "<<p2.first<<" Name: "<<p2.second.first<<" Age: "<<p2.second.second<<endl;
+
+    //Array of Pairs
+    pair<int, int> arr[] = {{2,3},{5,7},{11,13}};
+    cout<<arr[2].first;
+}
+
 int main(){
-    int a;
-    cout<<"Enter the value of a ";
-    cin>>a;
-    cout<<"The value of a is "<<a;
-    
-    int b = INT16_MAX;
-    cout<<"The max value of in possible is "<<b;
-    return 0;
+    explainPairs();
 }
